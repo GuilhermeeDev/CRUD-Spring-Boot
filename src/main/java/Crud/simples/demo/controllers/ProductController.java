3c57a7ff1493b221f10d636d8e6e892e1c9e3cd2
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2026-02-14 10:23:09 -0400

    (add) Controlle da entidade produto: Possue metodos CRUD basicos, sem tratamento de erros complexos, sem DTOs

---
+++ src/main/java/Crud/simples/demo/controllers/ProductController.java
@@ -0,0 +1,58 @@
+package Crud.simples.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import Crud.simples.demo.repository.ProductRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import Crud.simples.demo.entity.Product;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+@RestController
+@RequestMapping("/produtos")
+public class ProductController {
+    
+    // Conectando o repositório para acessar os dados dos produtos.
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping("/criar")
+    public Product criarProduto(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping("/listar")
+    public List<Product> listarTodosProdutos() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product listarPeloId(@RequestParam Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/{id}")
+    public Product atualizarProduto(@PathVariable Long id, @RequestBody Product product) {
+        Product _product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+        _product.setNome(product.getNome());
+        _product.setDescricao(product.getDescricao());
+        _product.setPreco(product.getPreco());
+        _product.setQuantidade(product.getQuantidade());
+        return productRepository.save(_product);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void deletarProduto(@PathVariable Long id){
+        productRepository.deleteById(id);
+    }
+
+}
